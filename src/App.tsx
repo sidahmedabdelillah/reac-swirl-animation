fbd4bdc96d1efad064522f1a037a0a7cdeb751a0
Abdou_Bentegar <s_bentegar@enst.dz>
2022-04-25 01:32:01 +0100

    pipe lines showcase

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ import TopBar from './components/TopBar'
 import PipeLinesPage from './page/PipeLinesPage'
 import SwirlPage from './page/SwirlPage'
 
-const baseName = process.env.REACT_APP_ENV === 'production' ?  '/reac-swirl-animation' : ''
+const baseName = process.env.NODE_ENV === 'production' ?  process.env.PUBLIC_URL : ''
 
 function App() {
+
+  console.log(process.env)
   return (
     <BrowserRouter basename={baseName} >
       <div className='App relative'>
